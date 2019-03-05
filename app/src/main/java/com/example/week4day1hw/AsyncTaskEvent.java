acbd50ebaba78c7bff46baf61871c065a1440531
consultant <dharris.csu@yahoo.com>
2019-03-05 09:01:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/week4day1hw/AsyncTaskEvent.java
@@ -0,0 +1,20 @@
+package com.example.week4day1hw;
+
+public class AsyncTaskEvent {
+
+    public class AsyncTaskEvent {
+        String message;
+
+        public AsyncTaskEvent(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+}
